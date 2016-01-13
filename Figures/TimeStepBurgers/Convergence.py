640a94be96a41e020cd437255be88b1591afc200
Truman Ellis <ellis.truman@gmail.com>
2016-01-13 15:05:02 -0600

    Added dissertation

---
+++ Figures/TimeStepBurgers/Convergence.py
@@ -0,0 +1,22 @@
+from pylab import *
+
+h = [
+	# 1./2,
+    1./4,
+    1./8,
+    1./16,
+    ]
+l2error = [
+# 0.0020879,
+0.000174044,
+1.18334e-05,
+7.53293e-07,
+    ]
+
+slope,intercept=polyfit(log(h),log(l2error),1)
+
+loglog(h, l2error, '-o', label='slope = '+"{:.2f}".format(slope))
+xlabel('h, dt')
+ylabel('error')
+legend(loc='best')
+show()
